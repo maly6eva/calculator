d73e4b4c8da06b2b2c7f4cffc68737ba651e90ef
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-04-05 07:25:49 +0300

    result

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,45 +1,68 @@
-import { useState } from "react";
+import {useState} from "react";
 import "./index.css";
 
 
 function Calculator() {
-  return (
-    <div className="calculator-container">
-      <h1 className="calculator-title">UseState Calculator</h1>
-      <div className="calculator">
-        <div className="display">0</div>
-        <div className="increment-buttons">
-          <button className="increment">+1</button>
-          <button className="decrement">-1</button>
+    const [count, setCount] = useState('0')
+
+    function subOne() {
+        const resultOne = eval(count)
+        setCount((resultOne + 1).toString())
+    }
+
+    function subTwo() {
+        const resultTwo = eval(count)
+        setCount((resultTwo - 1).toString())
+    }
+
+    function resultCount(value) {
+        if (value === 'C') {
+            setCount('0')
+        } else if (value === '=') {
+            setCount(eval(count).toString())
+        }
+
+        setCount((prev) => (prev === '0' ? value : prev + value))
+
+    }
+
+    return (
+        <div className="calculator-container">
+            <h1 className="calculator-title">UseState Calculator</h1>
+            <div className="calculator">
+                <div className="display">{count}</div>
+                <div className="increment-buttons">
+                    <button className="increment" onClick={subOne}>+1</button>
+                    <button className="decrement" onClick={subTwo}>-1</button>
+                </div>
+                <div className="buttons">
+                    <button onClick={() => resultCount(1)}>1</button>
+                    <button onClick={() => resultCount(2)}>2</button>
+                    <button onClick={() => resultCount(3)}>3</button>
+                    <button className="operator" onClick={() => resultCount('+')}>+</button>
+                    <button onClick={() => resultCount(4)}>4</button>
+                    <button onClick={() => resultCount(5)}>5</button>
+                    <button onClick={() => resultCount(6)}>6</button>
+                    <button className="operator" onClick={() => resultCount('-')}>-</button>
+                    <button onClick={() => resultCount(7)}>7</button>
+                    <button onClick={() => resultCount(8)}>8</button>
+                    <button onClick={() => resultCount(9)}>9</button>
+                    <button className="operator" onClick={() => resultCount('*')}>×</button>
+                    <button onClick={() => resultCount(0)}>0</button>
+                    <button onClick={() => resultCount(',')}>,</button>
+                    <button className="equals" onClick={() => resultCount('=')}>=</button>
+                    <button className="operator" onClick={() => resultCount('/')}>/</button>
+                    <button className="clear" onClick={() => setCount('0')}>C</button>
+                </div>
+            </div>
+            <div className="technologies-used">
+                <p>
+                    <strong>Technologies used:</strong> React, JSX, CSS Modules, JavaScript (useState, events
+                    handling)
+                </p>
+            </div>
         </div>
-        <div className="buttons">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button className="operator">+</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button className="operator">-</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="operator">×</button>
-          <button>0</button>
-          <button>,</button>
-          <button className="equals">=</button>
-          <button className="operator">÷</button>
-          <button className="clear">C</button>
-        </div>
-      </div>
-      <div className="technologies-used">
-        <p>
-          <strong>Technologies used:</strong> React, JSX, CSS Modules, JavaScript (useState, events
-          handling)
-        </p>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Calculator;
